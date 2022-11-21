10319fde41132063faabd620d7edc2b2975a0ce6
Nihel CHRAIEF <nchraief@spark-it.fr>
2022-11-22 00:51:20 +0100

    grab keypoint

--- public/lc-mouse-drag.js
+++ public/lc-mouse-drag.js
@@ -7,11 +7,11 @@
  */
 
 
-(function() { 
-   "use strict";
+(function () {
+    "use strict";
 
-    /* prevent multiple script inits */ 
-    if(typeof(window.lc_mouseDrag) == 'function') {
+    /* prevent multiple script inits */
+    if (typeof (window.lc_mouseDrag) == 'function') {
         return true;
     }
 
@@ -20,29 +20,29 @@
      * Public class initializing the plugin for each targeted element 
      * suggested ratio = 0.3
      */
-    window.lc_mouseDrag = function(attachTo, ratio = 0.3, ignoreX = false, ignoreY = false) {
-        if(!attachTo) {
+    window.lc_mouseDrag = function (attachTo, ratio = 0.3, ignoreX = false, ignoreY = false) {
+        if (!attachTo) {
             return console.error('You must provide a valid selector or DOM object as first argument');
         }
 
-        
+
         /* get elements to attach event to */
-        const get_elems = function(selector) {
-            if(typeof(selector) != 'string') {
-                return (selector instanceof Element) ? [selector] : Object.values(selector);   
+        const get_elems = function (selector) {
+            if (typeof (selector) != 'string') {
+                return (selector instanceof Element) ? [selector] : Object.values(selector);
             }
 
             // clean problematic selectors
-            (selector.match(/(#[0-9][^\s:,]*)/g) || []).forEach(function(n) {
+            (selector.match(/(#[0-9][^\s:,]*)/g) || []).forEach(function (n) {
                 selector = selector.replace(n, '[id="' + n.replace("#", "") + '"]');
             });
 
             return document.querySelectorAll(selector);
         };
 
-        
+
         /* perform */
-        const mouseDrag = function($elem) { 
+        const mouseDrag = function ($elem) {
             let trackX = (!ignoreX) ? true : false,
                 trackY = (!ignoreY) ? true : false,
 
@@ -50,10 +50,10 @@
                 curYPos = 0,
                 curXPos = 0,
 
-                startScrollY 	= 0,
-                startScrollX 	= 0,
-                scrollDif   	= 0,
-                animation       = null;    
+                startScrollY = 0,
+                startScrollX = 0,
+                scrollDif = 0,
+                animation = null;
 
 
             $elem.addEventListener('mousedown', (e) => {
@@ -64,72 +64,97 @@
                 startScrollX = parseInt($elem.scrollLeft, 10);
                 curYPos = e.clientY;
                 curXPos = e.clientX;
-            }); 
+            });
 
 
             $elem.addEventListener('mouseup', (e) => {
-                if(!ratio) {
-                    return true;    
+                if (!ratio) {
+                    return true;
                 }
-                
+
                 // smooth scroll
                 let currScrollY = $elem.scrollTop,
                     scrollDiffY = (startScrollY - currScrollY) * -1.5,
-                    newScrollY = currScrollY + ( scrollDiffY * ratio),
+                    newScrollY = currScrollY + (scrollDiffY * ratio),
 
                     currScrollX = $elem.scrollLeft,
                     scrollDiffX = (startScrollX - currScrollX) * -1.5,
-                    newScrollX = currScrollX + ( scrollDiffX * ratio);
+                    newScrollX = currScrollX + (scrollDiffX * ratio);
 
                 let scroll_obj = {
                     behavior: 'smooth'
                 };
-                if(trackY) {
+
+
+                if (trackY) {
                     scroll_obj.top = newScrollY;
+                    //  document.getElementById('keypoint').style.top = scroll_obj.top;
                 }
-                if(trackX) {
+                if (trackX) {
                     scroll_obj.left = newScrollX;
+                    // document.getElementById('keypoint').left = scroll_obj.left;
+                    // document.getElementById('keypoint').style.left = scroll_obj.left;
                 }
 
                 animation = $elem.scroll(scroll_obj);
+                // console.log(document.getElementById('keypoint').left +"t1")
+                // console.log(scroll_obj.left +"t2")
+
+
+
             });
 
-            
-            
+
+
             document.body.addEventListener('mouseup', (e) => {
                 curDown = false;
             });
-            
-            
+
+
 
             $elem.addEventListener('mousemove', (e) => {
-                if(curDown === true) {
-                    if(animation) {
-                        animation.pause();    
+                if (curDown === true) {
+                    if (animation) {
+                        animation.pause();
                     }
 
                     let scroll_obj = {
                         behavior: 'auto'
                     };
-                    if(trackY) {
+
+
+                    if (trackY) {
                         scroll_obj.top = startScrollY + (curYPos - e.clientY);
+                        document.getElementById('keypoint').top = scroll_obj.top;
+
                     }
-                    if(trackX) {
+                    if (trackX) {
                         scroll_obj.left = startScrollX + (curXPos - e.clientX);
+                        document.getElementById('keypoint').left = scroll_obj.left;
                     }
-                    
+                    let transform = {
+                        behavior: 'auto',
+                        top: document.getElementById('keypoint').top,
+                        left: document.getElementById('keypoint').left
+                    }
+                    var x = document.getElementById('keypoint').style.transform = "translate( " + transform.left + "px) ,(" + transform.top + "px))";
+
+                    console.log(x)
+
+
                     $elem.scroll(scroll_obj);
+                    //document.getElementById('keypoint').scroll(transform);
                 }
             });
         };
-        
+
 
         // init
         get_elems(attachTo).forEach(($el) => {
 
             // not for touch mobile devices
-            if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                document.body.classList.add('lc_mousedrag_is_mobile');  
+            if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                document.body.classList.add('lc_mousedrag_is_mobile');
                 return true;
             }
 
